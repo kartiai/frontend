77d9f25566d7bf94ea8eef21824069cf626da96b
Olaf4tza <73993173+DragosSofia@users.noreply.github.com>
2023-03-19 05:19:03 +0200

    Update link cos reload la deschidere cart

--- static/js/list.js
+++ static/js/list.js
@@ -47,6 +47,36 @@ function getDataPCgarage(a) {
     });
 }
 
+function getDataCel(a) {
+  fetch('/profile/data?firma=cel&username=' + a)
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+      products = data;
+      generateList();
+      // Do something with the data
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+}
+
+function getDataAltex(a) {
+  fetch('/profile/data?firma=altex&username=' + a)
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+      products = data;
+      generateList();
+      // Do something with the data
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+}
+
 function generateList(){
   document.getElementById("product-list-tag-id").innerHTML = '';
 for (let i = 0; i < products.length; i++) {
@@ -58,8 +88,10 @@ for (let i = 0; i < products.length; i++) {
             <div class="card-body">
                 <h5 class="card-title">${product.name}</h5>
                 <p class="card-text">${product.description}</p>
-                <a href="#" class="btn btn-primary">${product.price}</a>
-                <a href="#" class="btn btn-primary class-col-list-but-id" id="compare_data-ida1321" data-bs-toggle="modal" data-name-product="${product.name}" data-bs-target="#exampleModal">Compare</a>
+                <a href="${product.link}" class="btn btn-primary">${product.price}</a>
+                <a href="#" class="btn btn-primary class-col-list-but-id"
+                id="compare_data-ida1321" data-bs-toggle="modal" data-name-product="
+                ${product.name}" data-bs-target="#exampleModal">Compare</a>
             </div>
             </div>
             </div>
@@ -71,4 +103,8 @@ for (let i = 0; i < products.length; i++) {
 }
 }
 
-getDataAll();
+document.getElementById("setButton").click();
+
+
+
+
